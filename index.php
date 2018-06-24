3849f2c54e24aec648d1357c9fc4246d935cd392
Monty van Emmerik <mvanemmerik@me.com>
2018-06-24 11:24:24 -0400

    buttons

--- index.php
+++ index.php
@@ -6,11 +6,16 @@ require __DIR__.'/includes/db.php';
 include __DIR__.'/includes/header.html';
 ?>
 
-<ul>
-    <li><a href='mysql.php'>MySQL</a></li>
-    <li><a href='ms.php'>Microsoft SQL</a></li>
-    <li><a href='pg.php'>PostgreSQL</a></li>
-</ul>
+<a class="btn btn-primary btn-lg" href="mysql.php" role="button">MySQL</a>
+<a class="btn btn-secondary btn-lg" href="ms.php" role="button">Microsoft SQL</a>
+<a class="btn btn-success btn-lg" href="pg.php" role="button">PostgreSQL</a>
+<a class="btn btn-danger btn-lg" href="mysql.php" role="button">MySQL</a>
+<a class="btn btn-warning btn-lg" href="ms.php" role="button">Microsoft SQL</a>
+<a class="btn btn-info btn-lg" href="pg.php" role="button">PostgreSQL</a>
+<a class="btn btn-light btn-lg" href="mysql.php" role="button">MySQL</a>
+<a class="btn btn-dark btn-lg" href="ms.php" role="button">Microsoft SQL</a>
 
 <?php
 include __DIR__.'/includes/footer.html';
+
+
